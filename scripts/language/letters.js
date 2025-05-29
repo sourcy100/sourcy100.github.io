9920fab16931c01f1a1fc2a5faeecc97d05adda6
sourcy <dejdevatarovzena@gmail.com>
2025-05-29 23:03:11 +0500

    zs+hs

--- scripts/language/letters.js
+++ scripts/language/letters.js
@@ -2,14 +2,14 @@
 let ief = document.getElementById("ief")
 
 let draw = function (name) {
-    return `<li><img src="///sourcy100.github.io/language/${name}.png" width="auto" height="36"/> - ${name}</li>`
+    return `<li><img src="///sourcy100.github.io/language/${name}.png" width="auto" height="42"/> - ${name}</li>`
 }
 
 
 let f = [
     'ta', 'te', 'ti', 'tu', 'to', 't',
     'da', 'de', 'di', 'du', 'do', 'd',
-    'sa', 'se', 'ci', 'su', 'so', 's',
+    'sa', 'se', 'si', 'su', 'so', 's',
     'ba', 'be', 'bi', 'bu', 'bo', 'b',
     'ka', 'ke', 'ki', 'ku', 'ko', 'k',
     'na', 'ne', 'ni', 'nu', 'no', 'n',
@@ -17,7 +17,9 @@ let f = [
     'cha', 'che', 'chi', 'chu', 'cho', 'ch',
     'wa', 'we', 'wi', 'wu', 'wo', 'w',
     'la', 'le', 'li', 'lu', 'lo', 'l',
-    'a', 'e', 'i', 'o', 'u'
+    'ga', 'ge', 'gi', 'gu', 'go', 'g',
+    'za', 'ze', 'zi', 'zu', 'zo', 'z',
+    'a', 'e', 'i', 'o', 'u', "d'ore", "se'she"
 ]
 
 
--- scripts/language/words.js
+++ scripts/language/words.js
@@ -4,7 +4,7 @@ let words = document.getElementById("wrds")
 let repres = function (name) {
     let n = ''
     for (let j=0; j<name.length; j++){
-        n += `<img src="///sourcy100.github.io/language/${name[j]}.png" width="auto" height="36">`
+        n += `<img src="///sourcy100.github.io/language/${name[j]}.png" width="auto" height="42">`
     }
     return n
 }
