29488ba47aa9c72a3650302fcc2be88b8ea4f5de
sourcy <dejdevatarovzena@gmail.com>
2025-05-29 14:09:57 +0500

    Inital Commit

---
+++ scripts/header.js
@@ -0,0 +1,8 @@
+
+const header = document.getElementsByClassName("header")[0]
+
+header.innerHTML = 
+`
+<a href="https://sourcy100.github.io"><button>Sourcy</button></a>
+<a href="https://youtube.com/@Artswaigen"><button>Sourcy</button></a>
+`
