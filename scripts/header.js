0d3125eaf2380c2042d8b53e0da5a20c51c6f67f
sourcy <dejdevatarovzena@gmail.com>
2025-05-29 16:28:23 +0500

    make another <a /> class

--- scripts/header.js
+++ scripts/header.js
@@ -1,4 +1,13 @@
 
 let hs = document.getElementById("hs")
 
-hs.innerHTML = `<a href="https://sourcy100.github.io">Sourcy</a><a href="https://youtube.com/@Artswaigen">YouTube</a>`
+hs.innerHTML = `
+<a href="https://sourcy100.github.io" class="headache">
+Sourcy
+</a>
+<a href="https://youtube.com/@Artswaigen" class="headache">
+YouTube
+</a>
+<a href="https://sourcy100.github.io/language" class="headache">
+Language
+</a>`
