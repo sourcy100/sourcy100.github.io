a7946f71d80b1d7c429d73cf5c0c2b813ec33153
sourcy <dejdevatarovzena@gmail.com>
2025-05-29 14:36:34 +0500

    Fix stuff

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,7 @@
 
-const header = document.getElementsByClassName("header")[0]
+const header = document.getElementsByClassName("header")[0];
 
-header.innerHTML = 
-`
-<a href="https://sourcy100.github.io"><button>Sourcy</button></a>
-<a href="https://youtube.com/@Artswaigen"><button>Sourcy</button></a>
-`
+header.innerHTML = (`
+<a href="https://sourcy100.github.io">Sourcy</a>
+<a href="https://youtube.com/@Artswaigen">YouTube</a>
+`);
