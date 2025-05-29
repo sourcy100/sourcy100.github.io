42e26d59c767aac51f0f0ac70ede38c1e46bc8ba
sourcy <dejdevatarovzena@gmail.com>
2025-05-29 16:36:04 +0500

    css-html-js nav

--- scripts/header.js
+++ scripts/header.js
@@ -5,9 +5,12 @@ hs.innerHTML = `
 <a href="https://sourcy100.github.io" class="headache">
 Sourcy
 </a>
+
+<nav>
 <a href="https://youtube.com/@Artswaigen" class="headache">
 YouTube
 </a>
 <a href="https://sourcy100.github.io/language" class="headache">
 Language
-</a>`
+</a>
+</nav`
